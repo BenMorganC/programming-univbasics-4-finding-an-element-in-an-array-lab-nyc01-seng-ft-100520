764c70e2d8cec52ceae319decf041cd2739470b7
Ben Morgan-Cleveland <benmorgancleveland@gmail.com>
2020-09-17 16:01:56 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -2,7 +2,7 @@ def find_element_index(array, value_to_find)
   count = 0
   while array[count] < array.length do
     count += 1
-    puts array[count] == array[value_to_find]
+    puts array[count] == value_to_find
     return array[count]
   end
 end
